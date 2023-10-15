f9c7029b2e6eeec70e262267044481978bcab283
rohanmushan <97397494+rohanmushan@users.noreply.github.com>
2023-10-15 17:39:48 +0530

    Delete 8.py

--- 8.py
+++
@@ -1,4 +0,0 @@
-#Calculate the perimeter of a circle given its radius
-radius=float(input("Enter the radius of the circle: "))
-circumference=2*3.14*radius
-print(f"The circumference of the circle is: {circumference}")
