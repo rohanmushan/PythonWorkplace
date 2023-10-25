0d0e7eb9d3ac62dc41a5a380a21565832d35bbca
Rohan Mushan <97397494+rohanmushan@users.noreply.github.com>
2023-10-25 16:08:35 +0530

    51.py

--- 1-100/51.py
+++ 1-100/51.py
@@ -1,12 +1,5 @@
 # Find the union of two sets
-# Create two sets
-set1 = {1, 2, 3, 4, 5}
-set2 = {3, 4, 5, 6, 7}
-
-# Find the union of the two sets
+set1={1, 2, 3, 4, 5}
+sets={3, 4, 5, 6, 7}
 union_set = set1.union(set2)
-#or can use this below cmd
-#union_set = set1 | set2
-
-# Print the result
 print(union_set)
