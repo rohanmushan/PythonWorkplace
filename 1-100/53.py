dd181c763d94e06726d50e23403f4043f0472391
Rohan Mushan <97397494+rohanmushan@users.noreply.github.com>
2023-10-25 16:51:39 +0530

    Update 53.py

--- 1-100/53.py
+++ 1-100/53.py
@@ -1,10 +1,6 @@
 # Count the frequency of elements in a list using a dictionary
-# Sample list
-my_list = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
-
-# Create an empty dictionary to store the element frequencies
-element_count = {}
-
+my_list=[1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+element_count={}
 # Count the frequency of elements in the list
 for item in my_list:
     if item in element_count:
@@ -12,6 +8,5 @@ for item in my_list:
     else:
         element_count[item] = 1
 
-# Print the element frequencies
-for item, count in element_count.items():
-    print(f"{item}: {count}")
+# for item, count in element_count.items():
+#     print(f"{item}: {count}")
