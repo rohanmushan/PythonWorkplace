9e436d25cd9b13a2a2405093fb1a759a213931fa
Rohan Mushan <97397494+rohanmushan@users.noreply.github.com>
2023-10-25 16:24:33 +0530

    52.subset_of_another.py

--- 1-100/52.py
+++ 1-100/52.py
@@ -1,14 +1,10 @@
-# Check if a set is a subset of another set
-# Create two sets
-set1 = {1, 2, 3, 4, 5}
-set2 = {3, 4, 5}
+# Check if a set is a subset of another sets 
+set1={1, 2, 3, 4, 5}
+set2={3, 4, 5}
 
-# Check if set2 is a subset of set1
 is_subset = set2.issubset(set1)
 
 #OR
 # Check if set2 is a subset of set1 using the <= operator
-#is_subset = set2 <= set1
-
-# Print the result
+#is_subset=set2<=set1
 print(is_subset)
