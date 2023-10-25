5a427ac78269ff67b72b43263947e2a3ca1247d1
Rohan Mushan <97397494+rohanmushan@users.noreply.github.com>
2023-10-25 20:48:56 +0530

    Update and rename 37.py to 37_compound_interest.py

--- 1-100/37_compound_interest.py
+++ 1-100/37_compound_interest.py
@@ -1,8 +1,7 @@
 #Calculate compound interest
 
 def compound_interest(principal, rate, time):
-	# Calculates compound interest
-	Amount = principal * (pow((1 + rate / 100), time))
-	CI = Amount - principal
+	Amount=principal*(pow((1+rate/100),time))
+	CI=Amount-principal
 	print("Compound interest is", CI)
 compound_interest(10000, 10.25, 5)
