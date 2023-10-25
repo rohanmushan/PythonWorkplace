e083626eb3ab40dbdd274b937af0fe4e44e334a4
Rohan Mushan <97397494+rohanmushan@users.noreply.github.com>
2023-10-25 16:56:04 +0530

    Update 56.py

--- 1-100/56.py
+++ 1-100/56.py
@@ -1,22 +1,22 @@
 # Implement a simple address book using a dictionary.
-address_book = {}
+address_book={}
 def add_contact(name, address, phone):
-    address_book[name] = {'Address': address, 'Phone': phone}
-    print(f"Contact '{name}' added to the address book.")
+    address_book[name]={'Address':address,'Phone':phone}
+    print(f"Contact'{name}'added to the address book.")
 
 def view_contact(name):
     if name in address_book:
         contact_info = address_book[name]
-        print(f"Contact: {name}")
-        print(f"Address: {contact_info['Address']}")
-        print(f"Phone: {contact_info['Phone']}")
+        print(f"Contact:{name}")
+        print(f"Address:{contact_info['Address']}")
+        print(f"Phone:{contact_info['Phone']}")
     else:
-        print(f"'{name}' not found in the address book.")
+        print(f"'{name}'not found in the address book.")
 
 def list_contacts():
     print("Address Book:")
     for name, contact_info in address_book.items():
-        print(f"Name: {name}, Address: {contact_info['Address']}, Phone: {contact_info['Phone']}")
+        print(f"Name: {name},Address:{contact_info['Address']},Phone:{contact_info['Phone']}")
 
 def remove_contact(name):
     if name in address_book:
@@ -33,22 +33,22 @@ while True:
     print("4. Remove Contact")
     print("5. Quit")
 
-    choice = input("Enter your choice (1/2/3/4/5): ")
+    choice=input("Enter your choice (1/2/3/4/5): ")
 
-    if choice == '1':
-        name = input("Enter the name: ")
-        address = input("Enter the address: ")
-        phone = input("Enter the phone number: ")
+    if choice=='1':
+        name=input("Enter the name: ")
+        address=input("Enter the address: ")
+        phone=input("Enter the phone number: ")
         add_contact(name, address, phone)
-    elif choice == '2':
-        name = input("Enter the name to view: ")
+    elif choice=='2':
+        name=input("Enter the name to view: ")
         view_contact(name)
-    elif choice == '3':
+    elif choice=='3':
         list_contacts()
-    elif choice == '4':
-        name = input("Enter the name to remove: ")
+    elif choice=='4':
+        name=input("Enter the name to remove: ")
         remove_contact(name)
-    elif choice == '5':
+    elif choice=='5':
         break
     else:
         print("Invalid choice. Please select a valid option.")
