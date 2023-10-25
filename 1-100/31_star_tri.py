33a74f1f2e0b6e2335adf7d9f83b048a86c3e664
Rohan Mushan <97397494+rohanmushan@users.noreply.github.com>
2023-10-25 20:43:49 +0530

    Update and rename 31.py to 31_star_tri.py

--- 1-100/31_star_tri.py
+++ 1-100/31_star_tri.py
@@ -1,5 +1,6 @@
-#Print a pattern of numbers in an equilateral triangle.
-rows = int(input("Enter rows for the triangle: "))
+#Print a pattern of numbers in an equilateral triangle.
+
+rows=int(input("Enter rows for the triangle: "))
 for i in range(1, rows+1):
     print(" " * (rows - i), end="")
     for j in range(1, i+1):
