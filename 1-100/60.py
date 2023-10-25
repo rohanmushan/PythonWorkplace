41f398d0fefff14e80d96ede02b4d9a618aa161a
Rohan Mushan <97397494+rohanmushan@users.noreply.github.com>
2023-10-25 16:58:04 +0530

    Update 60.py

--- 1-100/60.py
+++ 1-100/60.py
@@ -1,16 +1,12 @@
 # Check if a string is a palindrome.
-# Function to check if a string is a palindrome
 def is_palindrome(input_string):
-    # Remove spaces and convert the string to lowercase for case-insensitive comparison
-    clean_string = input_string.replace(" ", "").lower()
+    clean_string=input_string.replace(" ", "").lower()
     
     # Check if the cleaned string is equal to its reverse
-    return clean_string == clean_string[::-1]
+    return clean_string==clean_string[::-1]
 
-# Input string
-my_string = "racecar"
 
-# Check if the string is a palindrome
+my_string="racecar"
 if is_palindrome(my_string):
     print(f"'{my_string}' is a palindrome.")
 else:
